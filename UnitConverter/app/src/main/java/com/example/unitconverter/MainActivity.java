0aa6030e2188ac95ec137bbbf38f5fb403e64060
Will Keller <wkeller3@binghamton.edu>
2019-08-30 20:02:43 -0400

    Everything works! Can convert between inches, feet, and yards. Other things will be added later

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.java
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Editable;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText input;
-    private Editable value;
+    private double value;
     private TextView answer;
     private Button b;
     private Spinner unit1;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         b = (Button)findViewById(R.id.button);
         unit1 = findViewById(R.id.unit1);
         unit2 = findViewById(R.id.unit2);
-        String[] items = new String[]{"in","ft","mi"};
+        String[] items = new String[]{"in","ft","yd"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         unit1.setAdapter(adapter);
         unit2.setAdapter(adapter);
@@ -38,10 +39,54 @@ public class MainActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                value = input.getText();
-                answer.setText(value);
+                value = Double.parseDouble(input.getText().toString());
+                //answer.setText(Double.toString(value));
+                String u1 = unit1.getSelectedItem().toString();
+                String u2 = unit2.getSelectedItem().toString();
+
+                if(u1.equals("in") && u2.equals("ft")){             //in -> ft
+
+                    answer.setText(Double.toString(value/12));
+
+                } else if(u1.equals("in") && u2.equals("yd")){      //in -> yd
+
+                    answer.setText(Double.toString(value/36));
+
+                } else if(u1.equals("ft") && u2.equals("in")){      //ft -> in
+
+                    answer.setText(Double.toString(value*12));
+
+                } else if(u1.equals("ft") && u2.equals("yd")){      //ft -> yd
+
+                    answer.setText(Double.toString(value/3));
+
+                } else if(u1.equals("yd") && u2.equals("in")){      //yd -> in
+
+                    answer.setText(Double.toString(value*36));
+
+                } else if(u1.equals("yd") && u2.equals("ft")){      //yd -> ft
+
+                    answer.setText(Double.toString(value*3));
+
+                } else{                                             //units equal, don't convert
+
+                    answer.setText(Double.toString(value));
+
+                }
             }
         });
+
+        /*unit1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                unit1.getSelectedItem().toString()
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });*/
 
     }
 }
